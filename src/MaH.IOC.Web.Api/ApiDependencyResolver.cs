e65fbf3eb10c1d2736c5f31d3f9e3840af98a19b
Mahdi Ahmadi <mah.ahmadi360@gmail.com>
2024-11-24 01:51:22 +0100

    Add Api service locator

---
+++ src/MaH.IOC.Web.Api/ApiDependencyResolver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dependencies;
+
+namespace MaH.IOC.Web.Api
+{
+    internal class ApiDependencyResolver : IDependencyResolver, IDependencyScope, IDisposable
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ApiDependencyResolver(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new ApiDependencyResolver(HttpContextServiceLocator.ServiceProvider);
+        }
+
+        public void Dispose()
+        {
+            _serviceProvider.Dispose();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (typeof(IHttpController).IsAssignableFrom(serviceType))
+            {
+                return _serviceProvider.GetService(serviceType);
+            }
+
+            try
+            {
+                return _serviceProvider.GetService(serviceType);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _serviceProvider.GetServices(serviceType);
+        }
+    }
+}
---
+++ src/MaH.IOC.Web.Api/ApiServiceResolverExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace MaH.IOC.Web.Api
+{
+    public static class ApiServiceResolverExtensions
+    {
+        public static void RegisterIOC(this HttpConfiguration config)
+        {
+            config.DependencyResolver = new ApiDependencyResolver(HttpContextServiceLocator.ServiceProvider);
+        }
+    }
+}
